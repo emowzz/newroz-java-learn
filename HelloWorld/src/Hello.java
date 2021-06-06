42942149d4c7cd466a5acca5546ea8d9402aca2d
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-06 19:17:27 +0600

    First commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello Fahim");
+
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber  * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("myTotal Number is = "+ myTotal);
+        //assignment
+        int myLastOne = 1000 - myTotal;
+        System.out.println("myLast Number is = "+myLastOne);
+    }
+}
