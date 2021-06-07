b0d97660497713b4994a2623c244376265b138da
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-07 19:06:45 +0600

    StatementsWhiteSpaceAndIndenting

---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50; //the entire line is the statement
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+
+        //the concept of indentation is to make code easy to read
+
+
+    }
+}
