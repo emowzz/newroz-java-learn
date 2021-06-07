01de4b11beaa22f0f7978bdc190e443f1a0f3635
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-07 17:33:17 +0600

    If & Ternary Conditions

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -5,46 +5,96 @@ public class Main {
     public static void main(String[] args) {
 
         int result = 1 + 2; // 1 + 2 = 3
-        System.out.println("1 + 2 = "+ result);
-        int previousResult  = result;
-        System.out.println("PreviousResult = "+ previousResult);
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("PreviousResult = " + previousResult);
         result = result - 1;
-        System.out.println("3 - 1 = "+ result);
-        System.out.println("PreviousResult = "+ previousResult);
+        System.out.println("3 - 1 = " + result);
+        System.out.println("PreviousResult = " + previousResult);
 
         result = result * 10; // 2 * 10 = 20
-        System.out.println("2 * 10 = "+ result);
+        System.out.println("2 * 10 = " + result);
 
         result = result / 5; //20 / 5 = 4
-        System.out.println("20 / 5 = "+ result);
+        System.out.println("20 / 5 = " + result);
 
         result = result % 3; // the remainder of (4 % 3) = 1
-        System.out.println("4 % 3 = "+ result);
+        System.out.println("4 % 3 = " + result);
 
         //result = result + 1;
         result++; // 1 + 1 = 2
-        System.out.println("1 + 1 = "+ result);
+        System.out.println("1 + 1 = " + result);
 
         result--; //2 - 1 = 1
-        System.out.println("2 - 1 = "+ result);
+        System.out.println("2 - 1 = " + result);
 
         //result = result + 2;
         result += 2;
-        System.out.println("1 + 2 = "+ result);
+        System.out.println("1 + 2 = " + result);
 
         //result = result * 10;
         result *= 10;
-        System.out.println("3 * 10 = "+ result);
+        System.out.println("3 * 10 = " + result);
 
         //result = result / 3;
         result /= 3; //30 / 3 = 10
-        System.out.println("30 / 3 = "+ result);
+        System.out.println("30 / 3 = " + result);
 
         //result = result - 2;
         result -= 2; //10 - 2 = 8
-        System.out.println("10 - 2 = "+ result);
+        System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
 
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90))
+        {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        // ternary operators
+        boolean wasCar = isCar ? true : false;
+
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        //challange
+        double firstValue = 20.00d;
+        double secondValue = 80.00d;
+        double eqn = (firstValue + secondValue) * 100.00d;
+        System.out.println(eqn);
+        double remainder = eqn % 40.00d;
+        System.out.println(remainder);
+
+        boolean boolVar = remainder == 0 ? true : false;
+        System.out.println(boolVar);
+        if (boolVar) {
+            System.out.println("Got some remainder");
+        }
 
     }
 }
