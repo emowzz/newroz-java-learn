474d313c6f3cbe21d981894f2f134bed5cd77566
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-07 12:47:06 +0600

    CharAndBoolean

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //char stands for only one character
+        char myChar = 'D';
+        char myUnicodeChar = '\u0995';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        //boolean stands for true or false
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
