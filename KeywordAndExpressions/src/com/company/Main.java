8bc3c7a63db082e7e15cff784b5973a7d9aeb3b9
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-07 18:40:08 +0600

    KeywordsAndExpressions

---
+++ KeywordAndExpressions/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        //variable names can not be like data types
+
+        int highScore = 50; //highScore = 50 is an expression here
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
