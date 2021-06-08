0009407d339c714dd3e58f4a2443d55e67434cf2
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-08 19:54:22 +0600

    Exercise MegaBytes Converter

--- SpeedConverter/src/Main.java
+++ SpeedConverter/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       long milles = SpeedConverter.toMilesPerHour(10.5);
-        System.out.println("Miles = "+milles);
+       long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = "+miles);
 
         SpeedConverter.printConversion(10.5);
     }
