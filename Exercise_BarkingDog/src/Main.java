204a63d5a783861ec6b36f6c8d1cbe5591b5b895
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-08 20:18:50 +0600

    Exercise Barking Dog

---
+++ Exercise_BarkingDog/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean wakeUpForBarking = BarkingDog.shouldWakeUp(true, 25);
+        System.out.println(wakeUpForBarking);
+    }
+}
