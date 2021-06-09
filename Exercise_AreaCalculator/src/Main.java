22c0d448d6a741f852190e3ae4e418e556e64f89
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-09 17:22:19 +0600

    Exercise Area Calculator Using MethodOverloading

---
+++ Exercise_AreaCalculator/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double circleArea = AreaCalculator.area(5.0);
+        System.out.println(circleArea);
+        double rectangleArea = AreaCalculator.area(4,5);
+        System.out.println(rectangleArea);
+    }
+}
