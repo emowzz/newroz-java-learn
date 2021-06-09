352d1cfc432688a993e5ae69a05b7b3689aa24d6
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-09 16:48:49 +0600

    SecondsAndMinutes using Method Overloading extra

--- SecondsAndMinutes_Challange/src/com/company/Main.java
+++ SecondsAndMinutes_Challange/src/com/company/Main.java
@@ -2,10 +2,13 @@ package com.company;
 
 public class Main {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
     public static void main(String[] args) {
 	// write your code here
         System.out.println(getDurationString(65, 45));
         System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-94));
+        System.out.println(getDurationString(65, 9));
 
     }
 
@@ -14,9 +17,28 @@ public class Main {
         {
             int hours = minutes / 60;
             int remainingMinutes =  minutes % 60;
-            return (hours+"h "+remainingMinutes+"m "+seconds+"s");
+
+            String hoursString = hours + "h";
+            if (hours < 10)
+            {
+                hoursString = "0"+hoursString;
+            }
+
+            String minutesString = remainingMinutes + "m";
+            if (remainingMinutes < 10)
+            {
+                minutesString = "0"+minutesString;
+            }
+
+            String secondsString = seconds + "s";
+            if (seconds < 10)
+            {
+                secondsString = "0"+secondsString;
+            }
+
+            return (hoursString+" "+minutesString+" "+secondsString);
         }
-        return "Invalid Value";
+        return INVALID_VALUE_MESSAGE;
     }
 
     public static String getDurationString(int seconds) {
@@ -26,6 +48,6 @@ public class Main {
             int remainingSeconds = seconds % 60;
             return (getDurationString(minutes, remainingSeconds));
         }
-        return "Invalid Value";
+        return INVALID_VALUE_MESSAGE;
     }
 }
