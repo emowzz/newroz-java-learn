afed9073aa1540590f9b4656ceac7803a7032f93
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-09 19:25:12 +0600

    Exercise Is Playing Cat

---
+++ Exercise_PlayingCat/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isPlayingCat = PlayingCat.isCatPlaying(false, 35);
+        System.out.println(isPlayingCat);
+    }
+}
