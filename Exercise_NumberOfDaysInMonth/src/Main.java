42a068b9bd14aad3db82b4542dafe392aede0e95
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-10 14:31:21 +0600

    Exercise Number of Days In Month

---
+++ Exercise_NumberOfDaysInMonth/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        boolean isLeapYear = NumberOfDaysInMonth.isLeapYear(2000);
+//        System.out.println(isLeapYear);
+        int DayOfMonth = NumberOfDaysInMonth.getDaysInMonth(-1, 2020);
+        System.out.println(DayOfMonth);
+    }
+}
