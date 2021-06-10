0fb6fe39569366438a6185bdd270834f2f6e4f41
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-10 12:35:09 +0600

    Exercise Day of The Week Challange

---
+++ Exercise_DayOfTheWeekChallange/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        DayOfTheWeekChallange.printDayOfTheWeek(9);
+        DayOfTheWeekChallenge_Using_IF_ELSE.printDayOfTheWeekUsingIfElse(5);
+    }
+}
