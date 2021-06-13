4717f523028d58789d3cb24cf07313f1d2ec8154
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-13 11:39:37 +0600

    Exercise Perfect Number

---
+++ Exercise_PerfectNumber/src/PerfectNumber.java
@@ -0,0 +1,17 @@
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1)
+            return false;
+
+        int i=1, sum=0;
+        while(i != number)
+        {
+            if (number % i == 0)
+                sum += i;
+            i++;
+        }
+        if (sum == number)
+            return true;
+        return false;
+    }
+}
