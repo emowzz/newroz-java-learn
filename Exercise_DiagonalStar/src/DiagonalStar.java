2508de07f8d5f922c9941c7939a6803f6ea4d4fe
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-14 15:11:30 +0600

    Exercise_DiagonalStar

---
+++ Exercise_DiagonalStar/src/DiagonalStar.java
@@ -0,0 +1,24 @@
+public class DiagonalStar {
+    public static void printSquareStar(int number)
+    {
+        if (number < 5)
+            System.out.println("Invalid Value");
+        else {
+            for (int row=0 ; row<number ; row++)
+            {
+                for(int column=0 ; column<number ; column++)
+                {
+                    if(row==0 || row==number-1 || column==0 ||column==number-1 || row==column || column == (number-(row+1)))
+                    {
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+            }
+        }
+
+    }
+}
