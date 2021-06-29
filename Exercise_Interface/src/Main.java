58e380d39ab6c6c2eeffd3b4ae38fa61eab61c9f
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-30 03:12:55 +0600

    Exercise Interfaces

---
+++ Exercise_Interface/src/Main.java
@@ -0,0 +1,68 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player emon = new Player("Emon", 20, 20);
+        System.out.println(emon.toString());
+        saveObject(emon);
+
+        emon.setHitPoints(8);
+        System.out.println(emon);
+        emon.setWeapon("Stormbringer");
+        saveObject(emon);
+        loadObject(emon);
+        System.out.println(emon);
+
+
+    }
+
+    public static ArrayList<String> readValues()
+    {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n"+
+                "1 to enter string\n"+
+                "0 to quit\n");
+
+        while (!quit)
+        {
+            System.out.println("Choose an option");
+            int choice = scanner.nextInt();
+            scanner.nextInt();
+            switch (choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave)
+    {
+        for (int i=0 ; i<objectToSave.write().size() ; i++)
+        {
+            System.out.println("Saving "+objectToSave.write().get(i)+" to storage device");
+        }
+    }
+
+    public static void loadObject(ISaveable objectToLoad)
+    {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+    }
+}
+
